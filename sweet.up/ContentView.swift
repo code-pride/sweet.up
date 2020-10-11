9026c57e0f279f932852c9c32ada1dcbebd063fd
Tomasz Ferens <tomaszferens@hotmail.com>
2020-10-11 12:22:04 +0200

    Initial Commit

---
+++ sweet.up/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sweet.up
+//
+//  Created by Tomasz Ferens on 11/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sweet.up/sweet_upApp.swift
@@ -0,0 +1,17 @@
+//
+//  sweet_upApp.swift
+//  sweet.up
+//
+//  Created by Tomasz Ferens on 11/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct sweet_upApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
