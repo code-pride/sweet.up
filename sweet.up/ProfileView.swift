0f8b11622bc4c2526f97715f922942e6e7a19c37
Marcin Krawczyk <marckraw@icloud.com>
2020-10-12 11:20:02 +0200

    Remove not needed piece of code

--- sweet.up/ProfileView.swift
+++ sweet.up/ProfileView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct ProfileView: View {
-  @State var sliderValue: Double = 50.0
   let primaryColor = Color(red: 176.0 / 255, green: 16.0 / 255, blue: 65.0 / 255, opacity: 1)
 
   // this is the modifier for common things - just for learning purposes
@@ -25,11 +24,6 @@ struct ProfileView: View {
       content.padding(10.0).border( /*@START_MENU_TOKEN@*/Color.black /*@END_MENU_TOKEN@*/)
         .accentColor(primaryColor)
     }
-  }
-
-  func doWhatever() -> String {
-    let myResponse: String = "dupa"
-    return myResponse
   }
 
   var body: some View {
@@ -63,14 +57,6 @@ struct ProfileView: View {
 
       Spacer()
 
-      Text("\(Int(self.sliderValue.rounded()))")
-
-      HStack {
-        Text("1")
-        Slider(value: self.$sliderValue, in: 1...100).accentColor(primaryColor)
-
-        Text("100")
-      }
     }
     .background(Image("Background"), alignment: .top)  // Background bleed from the top
     .padding(.top, -44)  // Background bleed from the top
