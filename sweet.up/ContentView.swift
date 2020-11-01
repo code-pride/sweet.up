7d8bb052500ac3e9173cc7075b2b958ab4ca33d2
Marcin Krawczyk <marckraw@icloud.com>
2020-11-01 13:48:56 +0100

    WIP

--- sweet.up/ContentView.swift
+++ sweet.up/ContentView.swift
@@ -9,44 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    TabView {
-      HomeView()
-        .tabItem {
-          VStack {
-            Image("Home")
-              .renderingMode(.original)
-            Text("Home")
-          }
-        }.tag(0)
-      PhotoAlbumView()
-        .tabItem {
-          VStack {
-            Image("Photo Album")
-              .renderingMode(.original)
-            Text("Album")
-          }
-        }.tag(1)
-      DiscoverAndMatchView()
-        .tabItem {
-          VStack {
-            Image("Discover & Match")
-              .renderingMode(.original)
-            Text("Discover & Match")
-          }
-        }.tag(2)
-      CalendarView()
-        .tabItem {
-          VStack {
-            Image("Calendar")
-              .renderingMode(.original)
-            Text("Calendar")
-          }
-        }.tag(3)
+    VStack {
+      HStack {
+        Image("Profile Photo4x").frame(height: 200)
+      }
+      TabView {
+        HomeView()
+          .tabItem {
+            VStack {
+              Image("Home")
+                .renderingMode(.original)
+              Text("Home")
+            }
+          }.tag(0)
+        PhotoAlbumView()
+          .tabItem {
+            VStack {
+              Image("Photo Album")
+                .renderingMode(.original)
+              Text("Album")
+            }
+          }.tag(1)
+        DiscoverAndMatchView()
+          .tabItem {
+            VStack {
+              Image("Discover & Match")
+                .renderingMode(.original)
+              Text("Discover & Match")
+            }
+          }.tag(2)
+        CalendarView()
+          .tabItem {
+            VStack {
+              Image("Calendar")
+                .renderingMode(.original)
+              Text("Calendar")
+            }
+          }.tag(3)
+      }
+      .onAppear(perform: {
+        UITabBar.appearance().barTintColor = UIColor(named: "MainBackgroundColor")
+      })
+      .font(.headline)
     }
-    .onAppear(perform: {
-      UITabBar.appearance().barTintColor = UIColor(named: "MainBackgroundColor")
-    })
-    .font(.headline)
   }
 }
 
